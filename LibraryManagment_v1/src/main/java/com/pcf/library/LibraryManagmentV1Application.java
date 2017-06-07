c7256db0d59d192d75f31227db14396e75d782b7
pgadiput <pgadiput@DIN66008968.corp.capgemini.com>
2017-06-07 15:03:05 +0530

    IntialVersion

---
+++ LibraryManagment_v1/src/main/java/com/pcf/library/LibraryManagmentV1Application.java
@@ -0,0 +1,12 @@
+package com.pcf.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagmentV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagmentV1Application.class, args);
+	}
+}
